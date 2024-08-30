54f491968449678e397b6981310f9b6ff66d7baa
novlarahmulia <rahmulianovla@gmail.com>
2024-08-30 09:21:01 +0700

    operator penugasan

--- 12  Operator Penugasan.php
+++ 12  Operator Penugasan.php
@@ -1,13 +1,21 @@
 <?php
-$speed = 83;
+$speed = 20;
+$speed2 =30 ;
 
 // ini opertor aritmatika
-$speed = $speed + 10;
+$speed = $speed + 30;
 
-// maka nilai speed akan samadengan 83 + 10 = 93
+// maka nilai speed akan samadengan 20 + 30 = 50
 
 // ini operator penugasan
-$speed += 10;
+$speed += 30;
 
-// sekarang nilai speed akan menjadi 93 + 10 = 103
+// sekarang nilai speed akan menjadi 30 + 30 = 60
+print($speed). "<hr>";
+$speed2 = $speed2 + 30;
+// dan
+$speed2 += 30;
+print($speed2);
+print_r("<p><strong>by :Novla Rahmulia");
+
 ?>
