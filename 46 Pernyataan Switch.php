ee7c74dd34495a7d06e7bf1cb32e127689a32307
novlarahmulia <rahmulianovla@gmail.com>
2024-08-30 12:59:58 +0700

    pernyataaan switch

---
+++ 46 Pernyataan Switch.php
@@ -0,0 +1,16 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+
+
+?>
