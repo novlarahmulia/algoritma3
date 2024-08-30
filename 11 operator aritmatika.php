33e9dffa36fbd786c88078d743de86a7a295a87d
novlarahmulia <rahmulianovla@gmail.com>
2024-08-30 09:05:05 +0700

    operator aritmatika

---
+++ 11 operator aritmatika.php
@@ -0,0 +1,39 @@
+<?php
+
+$a = 6;
+$b = 2;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// modulus Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+$hasil = (6 + 6) * 2; // Hasilnya adalah 24, bukan 20
+echo $hasil;
+echo "<hr>";
+
+?>
