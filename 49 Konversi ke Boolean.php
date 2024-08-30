a683e5a2227fa45ff549594c461cd156c0115026
novlarahmulia <rahmulianovla@gmail.com>
2024-08-30 13:17:41 +0700

    Konversi ke Boolean

---
+++ 49 Konversi ke Boolean.php
@@ -0,0 +1,10 @@
+<?php
+$nilai = 0;
+
+if ($nilai) {
+    echo "Variabel ini bernilai true";
+} else {
+    echo "Variabel ini bernilai false";
+}
+// Keluaran akan "Variabel ini bernilai false" karena nilai 0 dianggap false.
+?>
