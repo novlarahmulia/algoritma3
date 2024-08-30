3e7b7cf88620d2466c9afbdcefcdbdf53d123d11
novlarahmulia <rahmulianovla@gmail.com>
2024-08-30 10:34:35 +0700

    Operasi Matematika Dasar

---
+++ 33 Operasi Matematika Dasar.php
@@ -0,0 +1,19 @@
+<?php
+$angka1 = 5;
+$angka2 = 5;
+
+// Penambahan
+echo $angka1 + $angka2; // Hasil: 10
+
+// Pengurangan
+echo $angka1 - $angka2; // Hasil: 0
+
+// Perkalian
+echo $angka1 * $angka2; // Hasil: 25
+
+// Pembagian
+echo $angka1 / $angka2; // Hasil: 1
+print_r("<p><strong>by :Novla Rahmulia");
+
+
+?>
